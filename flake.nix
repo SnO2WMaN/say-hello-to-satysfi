69868a9fd4576cd2d1c5757aa90dee1033f70461
SnO2WMaN <me@sno2wman.net>
2022-06-08 12:33:02 +0900

    整理

--- flake.nix
+++ flake.nix
@@ -3,25 +3,30 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     devshell.url = "github:numtide/devshell";
+    satysfi-tools.url = "github:SnO2WMaN/satysfi-tools-nix";
 
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, flake-utils, devshell, ... } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShell =
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ devshell.overlay ];
-          };
-        in
-        pkgs.devshell.mkShell {
+  outputs = { self, nixpkgs, flake-utils, devshell, satysfi-tools, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlay
+            satysfi-tools.overlay
+          ];
+        };
+      in
+      {
+        devShell = pkgs.devshell.mkShell {
           imports = [
             (pkgs.devshell.importTOML ./devshell.toml)
           ];
         };
-    });
+      }
+    );
 }
